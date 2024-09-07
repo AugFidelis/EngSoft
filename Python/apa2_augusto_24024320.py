5c148ae0e4c9237f0e551cc23cba5d6f09117b07
AugFidelis <augustofidelis2003@gmail.com>
2024-09-07 20:12:22 -0300

    Python-APA2 Completa

--- Python/apa2_augusto_24024320.py
+++ Python/apa2_augusto_24024320.py
@@ -15,6 +15,7 @@ estudante = str(input())
 metodo_pag = str(input())
 
 if((hora_inicio == 18 and min_inicio >= 30) or hora_inicio > 18):
+    
     if(dia_semana == 1):
         ingresso = 30
         if(metodo_pag == 'C' and estudante != 'S'):
@@ -22,47 +23,54 @@ if((hora_inicio == 18 and min_inicio >= 30) or hora_inicio > 18):
         elif(estudante == 'S'):
             ingresso = ingresso - (ingresso * 0.5)
 
-    elif(dia_semana == 2):
+    elif(dia_semana == 2 or dia_semana == 3):
         ingresso = 20
-        if(metodo_pag == 'C' and estudante != 'S'):
-            ingresso = ingresso - (ingresso * 0.5)
-        elif(estudante == 'S'):
+        if(metodo_pag == 'C' or estudante == 'S'):
             ingresso = ingresso - (ingresso * 0.5)
 
-    elif(dia_semana == 3):
+    elif(dia_semana == 4 or dia_semana == 5):
         ingresso = 30
-        if(metodo_pag == 'C' and estudante != 'S'):
-            ingresso = ingresso - (ingresso * 0.3)
-        elif(estudante == 'S'):
-            ingresso = ingresso - (ingresso * 0.5)
-
-    elif(dia_semana == 4):
-        ingresso = 30
-        if(metodo_pag == 'C' and estudante != 'S'):
-            ingresso = ingresso - (ingresso * 0.3)
-        elif(estudante == 'S'):
-            ingresso = ingresso - (ingresso * 0.5)
-
-    elif(dia_semana == 5):
-        ingresso = 30
-        if(metodo_pag == 'C' and estudante != 'S'):
-            ingresso = ingresso - (ingresso * 0.3)
-        elif(estudante == 'S'):
+        if(metodo_pag == 'C' or estudante == 'S'):
             ingresso = ingresso - (ingresso * 0.5)
 
     elif(dia_semana == 6):
-        ingresso = 30
+        ingresso = 40
         if(metodo_pag == 'C' and estudante != 'S'):
             ingresso = ingresso - (ingresso * 0.3)
         elif(estudante == 'S'):
             ingresso = ingresso - (ingresso * 0.5)
 
     elif(dia_semana == 7):
+        ingresso = 40
+        if(metodo_pag == 'C' and estudante != 'S'):
+            ingresso = ingresso - (ingresso * 0.2)
+        elif(estudante == 'S'):
+            ingresso = ingresso - (ingresso * 0.5)
+
+else:
+
+    if(dia_semana == 1):
         ingresso = 30
         if(metodo_pag == 'C' and estudante != 'S'):
             ingresso = ingresso - (ingresso * 0.3)
         elif(estudante == 'S'):
             ingresso = ingresso - (ingresso * 0.5)
 
-else:
-    print("vespertino")
+    elif(dia_semana == 2 or dia_semana == 3 or dia_semana == 4):
+        ingresso = 15
+        if(metodo_pag == 'C' or estudante == 'S'):
+            ingresso = ingresso - (ingresso * 0.5)
+
+    elif(dia_semana == 5 or dia_semana == 6):
+        ingresso = 20
+        if(metodo_pag == 'C' or estudante == 'S'):
+            ingresso = ingresso - (ingresso * 0.5)
+
+    elif(dia_semana == 7):
+        ingresso = 30
+        if(metodo_pag == 'C' and estudante != 'S'):
+            ingresso = ingresso - (ingresso * 0.2)
+        elif(estudante == 'S'):
+            ingresso = ingresso - (ingresso * 0.5)
+
+print('Valor do ingresso: R$', format(ingresso, '.2f').replace('.', ','))
